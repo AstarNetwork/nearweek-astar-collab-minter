6e9d0b3bd8f70c7a8f8196e8c1b720ef401ee5a5
Gregory Luneau <gregory.luneau@gmail.com>
2023-09-24 20:52:30 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,98 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  useAddress,
+  useContract,
+  useMetadata,
+  useNFTBalance,
+  useNFT,
+  Web3Button,
+  ThirdwebNftMedia,
+} from "@thirdweb-dev/react";
 import "./styles/Home.css";
+import { setupWalletSelector } from "@near-wallet-selector/core";
+import { setupModal } from "@near-wallet-selector/modal-ui";
+import { setupNearWallet } from "@near-wallet-selector/near-wallet";
+import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
+import { setupHereWallet } from "@near-wallet-selector/here-wallet";
+import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
+import "@near-wallet-selector/modal-ui/styles.css";
+
+const selector = await setupWalletSelector({
+  network: "mainnet",
+  modules: [
+    setupNearWallet(),
+    setupMyNearWallet(),
+    setupHereWallet(),
+    setupMeteorWallet(),
+  ],
+});
+
+const modal = setupModal(selector, {
+  contractId: "mint.sharddog.near",
+});
+
+modal.show();
+
+let accounts = [];
+(async () => {
+  const wallet = await selector.wallet();
+  accounts = await wallet.getAccounts();
+  console.log("accounts", accounts);
+})();
 
 export default function Home() {
+  const address = useAddress();
+  const contractAddress = "0x8ebEae864d0E210811927308374A2bD11EeACA05";
+  const { contract } = useContract(contractAddress);
+  const { data: metadata, isFetched } = useMetadata(contract);
+  const {
+    data: ownerBalance,
+    isLoading,
+    error,
+  } = useNFTBalance(contract, address);
+  let balance = ownerBalance;
+console.log("balance", balance);
+
   return (
     <main className="main">
       <div className="container">
         <div className="header">
-          <h1 className="title">
-            Welcome to{" "}
-            <span className="gradient-text-0">
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className="description">
-            Get started by configuring your desired network in{" "}
-            <code className="code">src/index.js</code>, then modify the{" "}
-            <code className="code">src/App.js</code> file!
-          </p>
+          <h1 className="title">NEARWEEK Astar Collab Minter</h1>
 
           <div className="connect">
             <ConnectWallet
+              btnTitle="EVM Connect"
               dropdownPosition={{
                 side: "bottom",
                 align: "center",
               }}
             />
           </div>
-        </div>
 
-        <div className="grid">
-          <a
-            href="https://portal.thirdweb.com/"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-1">Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
+          <div className="connect">
+            {accounts.length > 0 ? (
+              balance && balance.eq(1) ? (
+                <div>Congratulations! You've already minted the NFT.</div>
+              ) : (
+                <Web3Button
+                  contractAddress={contractAddress}
+                  action={(contract) => contract.erc721.claim(1)}
+                >
+                  Mint
+                </Web3Button>
+              )
+            ) : (
+              <div>You must have the Astar NFT on Near to mint here</div>
+            )}
+          </div>
 
-          <a
-            href="https://thirdweb.com/dashboard"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-2">Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-3">Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
+          <div className="image">
+            {balance && balance.eq(1) & isFetched ? (
+              <ThirdwebNftMedia metadata={metadata} />
+            ) : (
+              <div>NFT not found</div>
+            )}
+          </div>
         </div>
       </div>
     </main>
--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,43 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "./styles/globals.css";
+import { Astar } from "@thirdweb-dev/chains";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const Shibuya = {
+  chainId: 81, // Chain ID of the network
+  // Array of RPC URLs to use
+  rpc: ["https://evm.shibuya.astar.network"],
+
+  // === Information for adding the network to your wallet (how it will appear for first time users) === \\
+  // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
+  nativeCurrency: {
+    decimals: 18,
+    name: "SBY",
+    symbol: "SBY",
+  },
+  icon: {
+    url: "ipfs://bafybeicniv63aec2i52c4hwa3hamud76a34skodqyyzsm3iasmu4lpibka/Shibuya_ring_white.png",
+    width: 1000,
+    height: 1000,
+    format: "png",
+  },
+  shortName: "shibuya", // Display value shown in the wallet UI
+  slug: "shibuya", // Display value shown in the wallet UI
+  testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
+  chain: "Shibuya", // Name of the network
+  name: "Shibuya Astar Testnet", // Name of the network
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <ThirdwebProvider
-      clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
+      clientId={import.meta.env.CLIENT_ID}
+      activeChain={Shibuya}
     >
       <App />
     </ThirdwebProvider>
